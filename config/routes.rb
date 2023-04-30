0a0f21e1ab265d7dbbf82734351d3ab4a18b0f81
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-04-30 20:23:13 +0430

    ADD ROUTES FOR MESSAGES

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'messages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "messages#index"
 end
