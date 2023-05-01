7d9869650091ca8faf81e70847a6e77cfc865142
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-05-01 22:39:55 +0430

    ADD REACT COMPONENTS

---
+++ app/javascript/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Greeting from './Greeting';
+
+function App() {
+  return (
+    <div>
+        <h3>Some Says</h3>
+        <Greeting />
+    </div>
+    );
+}
+
+export default App;
---
+++ app/javascript/components/Greeting.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Greeting({greeting}) {
+  return (<p>{greeting}</p>);
+}
+
+export default Greeting;
