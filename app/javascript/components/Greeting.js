b492006614b5c3b27fe2f467db7d47d405c6a125
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-05-05 23:17:15 +0430

    ADD GREETING COMPONENT

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
 
-function Greeting({greeting}) {
-  return (<p>{greeting}</p>);
+function Greeting() {
+  const greetings = useSelector((state) => state.greeting);
+  return (
+    <div>
+      <h1>Some One Says:</h1>
+      <h2>
+        {greetings}
+      </h2>
+    </div>
+  );
 }
 
 export default Greeting;
