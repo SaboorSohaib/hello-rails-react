16064023c701075d346b49806e3b37e41103ebb8
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-05-05 23:23:28 +0430

    IMPORT APP COMPONENT IN APPLICATION

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Entry point for the build script in your package.json
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "../components/App";
+import App from "./components/App";
 import { Provider } from "react-redux";
 import store from "../redux/store";
 
