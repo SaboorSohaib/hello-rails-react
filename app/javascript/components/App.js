336c249f394fd87b855f8a0fd0a410288e80b9b9
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-05-05 23:21:28 +0430

    IMPORT GREETING COMPONENT

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,7 +2,7 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchData } from './redux/greetings/greetings';
-import Greeting from './components/Greet';
+import Greeting from './components/Greeting';
 
 function App() {
   const dispatch = useDispatch();
